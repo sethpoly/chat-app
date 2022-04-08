69c511cb151f583baab30c8b13bf48141e865143
sethpoly <emailspolyniak@gmail.com>
2022-04-08 08:52:08 -0400

    restructure socket logic

--- backend/main.go
+++ backend/main.go
@@ -2,52 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/sethpoly/chat-app/pkg/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	// Check origin of connection
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// define a reader which will listen for new messages being sent to our Websocket endpoint
-func reader(conn *websocket.Conn) {
-	for {
-		// read in a message
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		// print out message for 'clarity'
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
 
 // define our Websocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	// upgrade this connection to a Websocket connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-
-	// listen indefinitely for new messages coming through socket
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
